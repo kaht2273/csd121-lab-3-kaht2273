c8f221003a266066a8820e6cfac9fc66d1112800
Trysten Kahtava <kaht2273@adsb.ca>
2023-04-03 10:26:06 -0400

    commit

--- src/main/java/practice_exercise_3_9/SavingsAccountTester.java
+++ src/main/java/practice_exercise_3_9/SavingsAccountTester.java
@@ -1,5 +1,13 @@
 package practice_exercise_3_9;
 
-public class SavingsAccountTester {
-    
+public class SavingsAccountTester
+{
+    public static void main(String[] args)
+    {
+        SavingsAccount TrystenAccount=new SavingsAccount(1000, 0.10);
+        TrystenAccount.addInterest();
+        System.out.println("Trysten balance="+ TrystenAccount.getBalance());
+    }
+
+
 }
